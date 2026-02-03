92982b64d37871c0bb90cdd7a9d06a71146e7dae
Tucker Brewer <brewert1@uci.edu>
2026-02-03 00:39:25 -0800

    round + hole_score class

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,7 @@
 from .course import Course
 from .hole import Hole
+from .hole_score import HoleScore
+from .round import Round
 from .tee import Tee
 
-__all__ = ["Course", "Hole", "Tee"]
+__all__ = ["Course", "Hole", "HoleScore", "Round", "Tee"]
